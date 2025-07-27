d498309ae147da4cecedee78827bd39e88fa649e
udud0099 <udud0099@gmail.com>
2025-07-27 18:01:36 +0545

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
- 
 export default function Home() {
   return (
     <>
-    <div>
-      <h1 className="text-4xl text-red-500 font-bold">ready</h1>
-    </div>
+      <div>
+        <h1 className="text-4xl text-red-500 font-bold">Intern Nepal - V2</h1>
+      </div>
     </>
   );
 }
